f72e3d1b9e291dbeedf4d2feba0acf6cee200723
falgranado <francisco.granado1@upr.edu>
2013-11-01 09:51:18 -0400

    Add cache manifest and the localStorage js file

--- localStorage.js
+++ localStorage.js
@@ -1,107 +1,315 @@
 var ls = window.localStorage;
 var setLetter = 65;
+var setRegex = /#[0123456789]*_[A-Z]*_[0123456789]*_#|set/;
+var containerCounter=1;
+var beginDate = $('#beginDate1');
 
-function initialize(){
-	//Check if browser supports localStorage
-	if(!Modernizr.localstorage){
-		alert("Your browser will not store data, please change or update your current browser");
-		return false;
-		}else if(Modernizr.localstorage){
-			alert("This application may store data in your device");
-		}
-	if(ls.length!= 0){
-		for(i=0;i<ls.length;i++){
-			getData(ls.key(i));
-			if($(ls.key(i)).is(':checkbox')==true){
-				//alert('if');
-			    $(ls.key(i)).prop('checked',true);
-				//alert("Checkbox");
+
+function initialize() {
+    //Check if browser supports localStorage
+    if (!Modernizr.localstorage) {
+        alert("This application will not work on your device, please change or update your current browser");
+		
+        return false;
+    } else if (Modernizr.localstorage) {
+        alert("This application may store data on your device");
+    }
+    if (ls.length != 0) {
+        for (i = 0; i < ls.length; i++) {
+            getData(ls.key(i));
+            if ($(ls.key(i)).is(':checkbox') == true) {
+                
+                $(ls.key(i)).prop('checked', true);
+                
+            }
+            console.log('Got'+' ' +this.key+' '+'from local storage');
+        }
+    }
+	$('#page2').on('pageinit', function(){
+	
+
+			$('#sampleProperties').validate({
+					rules:{
+						station: 'required',
+						date: 'required',
+						singleMultiContainer: 'required',
+						containerCuantity: {
+							required: 'true',
+							minlength: 1,
+							maxlength: 40
+						}
+					}
+				});
+		
+			$('#addSampleParameters').click(function (e) {
+				if($('#sampleProperties').valid()){
+				
+					if (ls.getItem('#singleMultiContainer') == 'multi') {
+						window.location = 'newSample.html#multiSet';
+			
+					} else if (ls.getItem('#singleMultiContainer') == 'single') {
+						window.location.href = '#setProperties';
+						changeSet(ls.getItem('#set'));
+						$('#multiAtributes').hide();
+					}
+					//Calls setButtons Function to add content to on SampleSet page
+					for(i=0;i<ls.getItem('#containerCuantity');i++){
+						setButtons(setLetter);
+						setLetter++
+					}
+				}
+			});
+	});
+	$('#setProperties').on('pageinit',function(){
+		
+		$('#setAtributesForm').validate({
+			rules:{
+					method:'required',
+					containersCuantity:{ 
+						required:true,
+						minlength:1,
+						maxlength:40
+					}
 			}
-			//alert(ls.key(i));
+		});
+		$('#analysesForm').validate({
+			rules:{
+				analysis: { 
+					required: true,
+					minlength: 1
+					}
+			},messages: {
+				analysis:{
+					required:'Please select at least one analysis'
+					}	
+			}
+		});
+		$('#linkToContainers').click(function (){
+		if($('#setAtributesForm').valid() && $('#analysesForm').valid()){
+			console.log('Forms multiAttributes and analysesForm were valid');
+			if(ls.getItem('#singleMultiContainer') == 'multi'){
+			$('#sampleParametersPageHeader').text('Set'+' '+ ls.getItem('#set')+', '+'container'+' '+ containerCounter);
+			}
+			if(ls.getItem('#singleMultiContainer')== 'single'){
+			$('#sampleParametersPageHeader').text('Single'+' '+'container'+' '+containerCounter);	
+			}
+			$.mobile.changePage('sampleParametersPage');
 		}
-	}
-	$('#addSampleInfo').click( function(e) {
-		
-		if(ls.getItem('#singleMultiContainer')== 'multi'){
-			window.location='newSample.html#multiSet';
-	
-		}else if(ls.getItem('#singleMultiContainer')== 'single'){
-			window.location.href = '#sampleInfo';
-			$('#multiAtributes').hide();
-		}
-	
-	});	
-	$('#addSampleSet').click(function(e) {
-		
-        setButtons(setLetter);
-		setLetter++;
-    });
-	saveSamples();
-	alert("initialize complete");
-}
-//Store the data as a key|value pair in localStorage
-function storeData(id,value){
-	ls.setItem('#'+id,value);
+		});
+	});
+	$('#sampleParametersPage').on('pageinit',function(){
+		$('#sampleParameters').validate({
+			rules:{
+				beginDate:'required',
+				beginTime:'required'
+			}
+			});	
+		$('#sampleParameters2').validate({
+			rules:{
+				P00061:'required',
+				P00010:'required',
+				P00063:'required',
+				P00065:'required'
+			}
+			});
+		$('#xmlNext').click(function () {
+			if($('#sampleParameters').valid() && $('#sampleParameters2').valid()){
+				if(ls.getItem('#singleMultiContainer') == 'multi'){
+					
+					$('#sampleParametersPageHeader').text('Set'+' '+ ls.getItem('#set')+', '+'container'+' '+ containerCounter);
+					containerCounter++;	
+				}else if(ls.getItem('#singleMultiContainer')=='single'){
+					
+					$('#sampleParametersPageHeader').text('Single'+' '+'container'+' '+containerCounter);
+					containerCounter++;	
+				}
+				$('html, body').animate({ scrollTop: 0 }, 0);
+			
+				loopTroughContainers();
+			}
+				
+		});
+	});
+    $('#addSampleSet').click(function (e) {
 
+        setButtons(setLetter);
+        setLetter++;
+    });
+
+	
+    $('#xmlNext').click(function () {
+		
+		if(ls.getItem('#singleMultiContainer') == 'multi'){
+			
+			$('#sampleParametersPageHeader').text('Set'+' '+ ls.getItem('#set')+', '+'container'+' '+ containerCounter);
+			containerCounter++;	
+		}else if(ls.getItem('#singleMultiContainer')=='single'){
+			
+			$('#sampleParametersPageHeader').text('Single'+' '+'container'+' '+containerCounter);
+			containerCounter++;	
+		}
+		$('html, body').animate({ scrollTop: 0 }, 0);
+	
+        loopTroughContainers();
+		
+		
+    });
+	//$('#beginDate').change(function(){
+	//	beginDate.val(this.value);
+	//});
+	
+}
+
+//Store the data as a key|value pair in localStorage
+function storeData(id, value) {
+    ls.setItem('#' + id, value);
+	console.log('Saved '+id+':'+value);
 }
 //get the data from localStorage
-function getData(id){
-	$(id).val(ls.getItem(id));
-	
+function getData(id) {
+    $(id).val(ls.getItem(id));
 }
 //Eliminates the data of the local Storage
-function rmvData(id){
-	ls.removeItem(id);
-	//alert("item removed");
+function rmvData(id) {
+    ls.removeItem(id);
 }
 //Validates Check boxes
-function chkBox(id,value){
-  // alert(id);
-   if(document.getElementById(id).checked == true){
-	  // alert('true');
-	   storeData(id,value);
-   }else if(document.getElementById(id).checked == false){
-	 //  alert('false');
-	   rmvData('#'+id);
-   }	
+function chkBox(id, value) {
+    if (document.getElementById(id).checked == true) {
+        storeData(id, value);
+    } else if (document.getElementById(id).checked == false) {
+        rmvData('#' + id);
+    }
 }
 //Sets the title of the header page
-function setText(id,value){
-	
+function setTitle(id, value) {
+	storeData(id,value);
+	$('#page2Header').text(ls.getItem('#'+id));
+	if(ls.getItem('#'+id) == 'Bedload'){
+		$('.suspendedAdditionalFields, .bottomAdditionalFields').hide();
+	}else if (ls.getItem('#'+id) == 'Bottom Material'){
+		$('.suspendedAdditionalFields, .bedloadAdditionalFields, #bedloadAdditionalFields').hide();	
+	}else if(ls.getItem('#'+id) == 'Suspended Sediment'){
+		$('.bedloadAdditionalFields, #bedloadAdditionalFields, .bottomAdditionalFields').hide();	
+	}
 }
 //Create button elements and appends it to the div block
-function createButton(buttonName){
-    var button = '<a href="#sampleInfo" onclick="setText(sampleInfoHeader,Set'+ ' '+buttonName+'")'+' class="ui-btn ui-btn-corner-all ui-shadow ui-btn-up-c" data-role="button" data-theme="c"'+''+'id="'+ buttonName+'"' + '>' + 
-               '<span class="ui-btn-inner ui-btn-corner-all">' + 
-                 '<span class="ui-btn-text">'+'Set'+ ' '+ buttonName + '</span>' + 
-               '</span>' +
-             '</a>';
-			 $('#sampleSets').append(button);		
-}
-function removeButton(buttonName){
-	
-	
-}
-//Creates buttons depending on the number entered by the user
-function setButtons(cuantity){
-	//var sampleLetter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-		  createButton(String.fromCharCode(setLetter));
-	 
+function createButton(buttonName) {
+    var button = '<a href="#setProperties" onClick="changeSet(this.id)" class="ui-btn ui-btn-corner-all ui-shadow ui-btn-up-c" data-role="button" data-theme="c"' + '' + 'id="' + buttonName + '"' + '>' +
+        '<span class="ui-btn-inner ui-btn-corner-all">' +
+        '<span class="ui-btn-text">' + 'Set' + ' ' + buttonName + '</span>' +
+        '</span>' +
+        '</a>';
+    $('#sampleSets').append(button);
 }
 
-function saveSamples(setId,containerId,parameters){
-	var parameters=[];
-	for(i=0;i<ls.length;i++){
-		parameters[i]= ls.key(i)+'='+'"'+ls.getItem(ls.key(i))+'"';
-			alert(parameters);
-	}
-		
-	storeData('SET_A',parameters);	
+function removeButton(buttonName) {
+
+
 }
+
+function getSample(set) {
+    for (i = 0; i < ls.length; i++) {
+        if (setRegex.test(ls.key(i))) {
+            var param = getData(ls.key(i));
+            if ($(ls.key(i)).is(':checkbox') == true) {
+                $(ls.key(i)).prop('checked', true);
+            }
+        }
+    }
+}
+//Changes the set and the header
+function changeSet(id) {
+	if(ls.getItem('#singleMultiContainer') == 'multi'){
+    storeData('set', id);
+	$('#setHeader').text('Set'+' '+id);
+	}
+	else if(ls.getItem('#singleMultiContainer') == 'single'){
+	storeData('set','SNGL');
+	ls.setItem('#containersCuantity',ls.getItem('#containerCuantity'));
+	$('#setHeader').text(ls.getItem('#containerCuantity')+' '+'single container(s)');
+	
+		
+	}
+}
+
+//Creates buttons depending on the number entered by the user
+function setButtons(cuantity) {
+    createButton(String.fromCharCode(setLetter));
+}
+
+//Save all samples parameters
+function saveSamples(station,setId, containerId) {
+    for (i = 0; i < ls.length; i++) {
+        if (!setRegex.test(ls.key(i))) {
+            storeData(station+'_'+setId + '_' + containerId + '_' + ls.key(i), ls.getItem(ls.key(i)));
+        }
+    }
+}
+//loop trought the desired amount of container
+function loopTroughContainers(){
+	 
+	if(containerCounter<=ls.getItem('#containersCuantity')){
+				saveSamples(ls.getItem('#station'),ls.getItem('#set'), containerCounter);
+				$.mobile.changePage('#sampleParametersPage');
+	}
+				if(containerCounter > ls.getItem('#containersCuantity')){
+					saveSamples(ls.getItem('#station'),ls.getItem('#set'), containerCounter);
+					if(ls.getItem('#singleMultiContainer') == 'multi'){
+					$.mobile.changePage('#multiSet');
+					}else if(ls.getItem('#singleMultiContainer') == 'single'){
+						$.mobile.changePage('#HomePage');
+					}
+					containerCounter= 1;
+				}	
+	rmvData(ls.getItem('#beginTime'));
+}
+function addOnLogic(){
+	if($('#analysesZ:checked') == true){
+		$('#fullSizeFraction').hide();	
+	}
+}
+//Determines User type
+function login(text){
+	storeData('userType',text);
+	//alert(text);
+	if(ls.getItem('#userType') == 'Observer'){
+		//alert(text);
+		$(' .hide,.div div select ui-block-c,.ui-block-d').hide();
+		
+		$('#messageToLab').attr('placeholder','Remarks');
+	}
+}
+
 
 
 
 //Loads localStorage to set defaults 
-$(document).ready(function(e) {
-    initialize();
+$(document).ready(function (e) {
+ 	//login(ls.getItem('#userType'));
+	//ls.clear();
+	initialize();
+	
+	
+  });
+$('#page2').bind('pageinit', function(){
+	
 });
+/*$('#page2').bind('pageinit',function(){
+  $('#sampleProperties').validate({
+	rules:{
+		station: 'required',
+		date: 'required',
+		singleMultiContainer: 'required',
+		containerCuantity:{
+			required:'true',
+			minlength:1,
+			maxlength:40
+		}
+	},
+	 submitHandler: function(form) {
+   					alert('Success!');
+  					}
+			
+  });
+ });*/
